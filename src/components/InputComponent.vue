807baf8894268dc42011eb84a2b0215d27f2d16a
CasperBvV <71759357+CasperBvV@users.noreply.github.com>
2026-01-26 15:42:58 +0100

    Enhance InputComponent with radio and label support
    Added support for 'radio' input type and optional 'label' and 'default' props. Improved input initialization to use 'default' if provided, and updated template to handle radio buttons and dynamic labels.

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -4,6 +4,8 @@ import { ref, watch } from 'vue'
 const props = defineProps<{
   inputType: string
   options?: string[]
+  default?: string
+  label?: string
   placeholder?: string
 
   value: string
@@ -14,7 +16,7 @@ const emit = defineEmits<{
   (e: 'update', value: string): void
 }>()
 
-const userInput = ref<string>(props.prefilled || '')
+const userInput = ref<string>(props.prefilled || props.default || '')
 
 // check if userInput changes and emit update event
 watch(userInput, (newValue) => {
@@ -23,12 +25,18 @@ watch(userInput, (newValue) => {
 </script>
 
 <template>
-  <div class="input-component">
-    <label for="user-input" v-if="label">{{ label }}:</label>
+  <div class="radio" v-if="inputType == 'radio'">
+    <label v-for="option in options" :key="option">
+      <input type="radio" :name="value" :value="option" v-model="userInput" :checked="userInput === option" />
+      {{ option }}
+    </label>
+  </div>
+  <div class="input-component" v-else>
+    <label :for="value" v-if="label">{{ label }}:</label>
     <select :name="value" :id="value" v-model="userInput" v-if="inputType === 'select' && options">
       <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
     </select>
 
-    <input :type="inputType" id="user-input" v-model="userInput" v-else />
+    <input :type="inputType" :id="value" v-model="userInput" :placeholder="props.placeholder" v-else />
   </div>
 </template>
