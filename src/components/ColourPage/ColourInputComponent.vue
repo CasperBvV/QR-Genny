b237532164e7401dadb7bd6329281d9ea49a710d
CasperBvV <71759357+CasperBvV@users.noreply.github.com>
2026-01-26 15:48:39 +0100

    Add gradient color option to ColourInputComponent
    Introduces a radio input to select between 'solid' and 'gradient' color types. When 'gradient' is selected, a second color input appears. Also adds basic styling for color input layout.

--- src/components/ColourPage/ColourInputComponent.vue
+++ src/components/ColourPage/ColourInputComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { qrData, type colorType } from '@/main'
 import InputComponent from '@/components/InputComponent.vue';
+import { ref } from 'vue';
 
 const props = defineProps<{
   value: 'dots' | 'background' | 'corners' | 'cornerDots'
@@ -23,14 +24,32 @@ function updateValue(newValue: string, field?: string) {
     ...qrData.value.colour[props.value],
     [field]: newValue,
   }
+
+  if (field === 'type') {
+    isGradient.value = newValue === 'gradient';
+  }
+
 }
 
 const currentData = qrData.value.colour ? qrData.value.colour[props.value] : {} as colorType;
+const isGradient = ref<boolean>(currentData?.type === 'gradient');
 
 </script>
 
 <template>
   <InputComponent inputType="radio" value="type" :options="['solid', 'gradient']" default="solid"
     :prefilled="currentData?.type" @update="updateValue" />
-  <InputComponent inputType="color" value="color1" :prefilled="currentData?.color1" @update="updateValue" />
+
+  <div class="row">
+    <InputComponent inputType="color" value="color1" :prefilled="currentData?.color1" @update="updateValue" />
+    <InputComponent v-if="isGradient" inputType="color" value="color2" :prefilled="currentData?.color2"
+      @update="updateValue" />
+  </div>
 </template>
+
+<style>
+.row {
+  display: flex;
+  gap: 1rem;
+}
+</style>
